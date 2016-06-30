44c6aee8f5625f5a4acb99c9f0cfc3d1c93886eb
shgtkshruch <shgtk.shruch@gmail.com>
2016-06-30 15:27:42 +0900

    円周上に点を描画してみよう

--- build/source/dotinstallBeautifulSphere.java
+++ build/source/dotinstallBeautifulSphere.java
@@ -14,6 +14,8 @@ import java.io.IOException;
 
 public class dotinstallBeautifulSphere extends PApplet {
 
+float radius = 150;
+
 public void setup() {
   
   
@@ -22,6 +24,17 @@ public void setup() {
 public void draw() {
   background(255);
   translate(width/2, height/2, 0);
+  rotateY(frameCount * 0.02f);
+
+  for (int dPhi = 0; dPhi < 360; dPhi += 10) {
+    float phi = radians(dPhi);
+    float x = radius * cos(phi);
+    float y = radius * sin(phi);
+
+    stroke(0);
+    strokeWeight(4);
+    point(x, y, 0);
+  }
 }
   public void settings() {  size(640, 360, P3D);  pixelDensity(displayDensity()); }
   static public void main(String[] passedArgs) {
--- dotinstallBeautifulSphere.pde
+++ dotinstallBeautifulSphere.pde
@@ -1,3 +1,5 @@
+float radius = 150;
+
 void setup() {
   size(640, 360, P3D);
   pixelDensity(displayDensity());
@@ -6,4 +8,15 @@ void setup() {
 void draw() {
   background(255);
   translate(width/2, height/2, 0);
+  rotateY(frameCount * 0.02);
+
+  for (int dPhi = 0; dPhi < 360; dPhi += 10) {
+    float phi = radians(dPhi);
+    float x = radius * cos(phi);
+    float y = radius * sin(phi);
+
+    stroke(0);
+    strokeWeight(4);
+    point(x, y, 0);
+  }
 }
